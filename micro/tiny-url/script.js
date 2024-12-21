df4f27bd52be207d784944cc426a17e18326388d
sumit-munda <166206537+sumit-munda@users.noreply.github.com>
2024-12-21 21:54:12 +0530

    Delete micro/tiny-url directory

--- micro/tiny-url/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("shorten-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const formData = new FormData(e.target);
-  const url = formData.get("url");
-  const custom_url = formData.get("custom-url");
-
-  console.log(url, custom_url);
-});
