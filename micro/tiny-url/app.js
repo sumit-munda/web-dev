16e5f0c4261ed53d89c51d6d9cd84d29d059ad33
sumit-munda <sumitkmunda.563@gmail.com>
2024-12-24 17:02:11 +0530

    app05

--- micro/tiny-url/app.js
+++ micro/tiny-url/app.js
@@ -1,9 +1,12 @@
 const fs = require("fs/promises");
 const http = require("http");
 const path = require("path");
+const crypto = require("crypto");
 
 const PORT = 7000;
-const serverFile = async (res, filepath, contentType) => {
+const DATA_FILE = path.join("data", "links.json");
+
+const serveFile = async (res, filepath, contentType) => {
   try {
     const data = await fs.readFile(filepath);
     res.writeHead(200, { "Content-Type": contentType });
@@ -14,23 +17,84 @@ const serverFile = async (res, filepath, contentType) => {
   }
 };
 
+const loadLinks = async () => {
+  try {
+    const data = await fs.readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.writeFile(DATA_FILE, JSON.stringify({}), "utf-8");
+      return {};
+    }
+    console.error(error);
+    return {};
+  }
+};
+
+const saveLinks = async (links) => {
+  try {
+    await fs.writeFile(DATA_FILE, JSON.stringify(links, null, 2), "utf-8");
+  } catch (error) {
+    console.error("Error saving links, ", error);
+  }
+};
+
 const server = http.createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
-      return serverFile(
-        res,
-        path.join(__dirname, "public", "index.html"),
-        "text/html"
-      );
+      return serveFile(res, path.join("public", "index.html"), "text/html");
+    } else if (req.url === "/style.css") {
+      return serveFile(res, path.join("public", "style.css"), "text/css");
+    } else if (req.url === "/links") {
+      const links = await loadLinks();
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(links));
     }
-  } else if (req.method === "GET") {
-    if (req.url === "/style.css") {
-      return serverFile(
-        res,
-        path.join(__dirname, "public", "style.css"),
-        "text/css"
-      );
-    }
+  }
+
+  if (req.method === "POST" && req.url === "/shorten") {
+    const links = await loadLinks();
+
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", async () => {
+      try {
+        console.log(body);
+        const { url, shortCode } = JSON.parse(body);
+
+        if (!url) {
+          res.writeHead(400, { "Content-Type": "text/plain" });
+          return res.end("URL is required!");
+        }
+
+        const finalShortCode =
+          shortCode || crypto.randomBytes(4).toString("hex");
+
+        if (links[finalShortCode]) {
+          res.writeHead(400, { "Content-Type": "text.plain" });
+          return res.end(
+            "Short code already exists. Pleaser choose anoter one."
+          );
+        }
+
+        links[finalShortCode] = url;
+        await saveLinks(links);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Server Error!");
+        console.error(error);
+      }
+    });
+
+    req.on("error", (err) => {
+      console.error("Error processing request", err);
+      res.writeHead(500, { "Content-Type": "text/plalin" });
+      res.end("Server Error!");
+    });
   }
 });
 
--- micro/tiny-url/public/script.js
+++ micro/tiny-url/public/script.js
@@ -1,9 +1,27 @@
-document.getElementById("shorten-form").addEventListener("submit", (e) => {
+document.getElementById("shorten-form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const formData = new FormData(e.target);
   const url = formData.get("url");
-  const custom_url = formData.get("custom-url");
+  const custom_url = formData.get("shortCode");
 
   console.log(url, custom_url);
+
+  try {
+    const response = await fetch('/shorten', {
+      method : "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({url, shortCode})
+    });
+
+    if(response.ok) {
+      alert("Form submitted successfully");
+    } else {
+      const errorMessage = await response.text();
+      alert(errorMessage);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  
 });
